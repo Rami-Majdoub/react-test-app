a8bb4614ac847b02688d03dce3e1c2128e551d0a
Rami-Majdoub <rami.majdoub1@gmail.com>
2022-03-16 22:24:26 +0100

    create ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} width="100px" />
+      <h1>Calculator</h1>
+      <br />
+      <input type={"text"} />
+      <br />
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <br/>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <br/>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <br/>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <br/>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+
+    
     </div>
   );
 }
---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import "./Button.css"
+
+function Button(){
+	return(
+		<button className="button">1</button>
+	);
+}
+
+export default Button;
